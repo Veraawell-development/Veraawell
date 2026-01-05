3326fed2705e3ad24468bfb92ab4f0efe5fdbdd2
AbhigyanRaj <Abhigyanraj673@gmail.com>
2026-01-05 18:15:40 +0530

    Fix: Call startServer() in index.js to bind port on Render

--- server/index.js
+++ server/index.js
@@ -1,1 +1,12 @@
-require('./server.js');
+/**
+ * Server Entry Point
+ * This file is the main entry point for the application
+ */
+
+const { startServer } = require('./server.js');
+
+// Start the server
+startServer().catch((error) => {
+    console.error('Failed to start server:', error);
+    process.exit(1);
+});
