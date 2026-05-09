0da7e4a3601db4c4495f6d1375d5313ec6f0b899
AbhigyanRaj <Abhigyanraj673@gmail.com>
2026-05-10 04:09:55 +0530

    Use explicit Gmail SMTP settings to fix production email sending

--- server/services/email.service.js
+++ server/services/email.service.js
@@ -25,7 +25,9 @@ function initializeTransporter() {
   }
 
   transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true, // Use SSL
     auth: {
       user: emailUser,
       pass: emailPass
